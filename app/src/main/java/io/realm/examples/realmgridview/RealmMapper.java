dd3c7e41d4cc9b75d8ba999b4d759b4221645c55
Tudor Luca <luca.mtudor@gmail.com>
2015-08-28 12:26:09 +0300

    Adds a detachResults method to "detach" query results from a Realm.

--- app/src/main/java/io/realm/examples/realmgridview/RealmMapper.java
+++ app/src/main/java/io/realm/examples/realmgridview/RealmMapper.java
@@ -3,8 +3,12 @@ package io.realm.examples.realmgridview;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.RealmResults;
 
 /**
  * Created by Tudor Luca on 27/08/15.
@@ -18,7 +22,11 @@ public abstract class RealmMapper {
 
     @SuppressWarnings("unchecked")
     public <T extends RealmObject> RealmList<T> detachList(final RealmList<T> list) {
-        final T[] array = (T[]) new RealmObject[list.size()];
+        final T[] array = (T[]) new RealmObject[0];
         return new RealmList<>(list.toArray(array));
+    }
+
+    public <T extends RealmObject> List<T> detachResults(final RealmResults<T> results) {
+        return new CopyOnWriteArrayList<>(results);
     }
 }
